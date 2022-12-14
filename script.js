b155b14679ba20c2cca297f73ae501266703b697
Ricardo Rosa <rrricardo00@yahoo.com.br>
2022-12-14 21:00:01 +0000

    commit

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 "use strict";
-const total = 20;
-function somar(a, b) {
-    return a + b;
+// const total: number = 20
+// function somar(a: number, b: number){
+//     return a + b
+// }
+// console.log(somar(5,15))
+// const carro = {
+//     marca: 'Honda',
+//     ano: 2014
+// }
+// carro.marca = "Audi"
+// const nintendo = {
+//     nome: "Nintendo",
+//     preco: "2000"
+// }
+// const trasnformPrice = (produto: {nome: string; preco: string}) => {
+//     produto.preco = 'R$ ' + produto.preco
+//     return produto
+// }
+// const newProduct = trasnformPrice(nintendo)
+const textao = " Aqui vai um textaoo    ";
+function normalizarTexto(texto) {
+    return texto.trim().toLocaleLowerCase();
 }
-console.log(somar(5, 10));
+console.log(normalizarTexto(textao));
--- script.ts
+++ script.ts
@@ -1,7 +1,34 @@
-const total: number = 20
+// const total: number = 20
 
-function somar(a: number, b: number){
-    return a + b
+// function somar(a: number, b: number){
+//     return a + b
+// }
+
+// console.log(somar(5,15))
+
+// const carro = {
+//     marca: 'Honda',
+//     ano: 2014
+// }
+
+// carro.marca = "Audi"
+
+// const nintendo = {
+//     nome: "Nintendo",
+//     preco: "2000"
+// }
+
+// const trasnformPrice = (produto: {nome: string; preco: string}) => {
+//     produto.preco = 'R$ ' + produto.preco
+//     return produto
+// }
+
+// const newProduct = trasnformPrice(nintendo)
+
+const textao = " Aqui vai um textaoo    "
+
+function normalizarTexto(texto: string) {
+    return texto.trim().toLocaleLowerCase()
 }
 
-console.log(somar(5,10))
+console.log(normalizarTexto(textao))
