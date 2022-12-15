19cec6d40d56bf452087f7158153c58536d80290
rrricardo00 <rrricardo00@yahoo.com.br>
2022-12-15 01:17:55 -0300

    ts

--- script.ts
+++ script.ts
@@ -25,10 +25,55 @@
 
 // const newProduct = trasnformPrice(nintendo)
 
-const textao = " Aqui vai um textaoo    "
+// const textao = " Aqui vai um textaoo    "
 
-function normalizarTexto(texto: string) {
-    return texto.trim().toLocaleLowerCase()
-}
+// function normalizarTexto(texto: string) {
+//     return texto.trim().toLocaleLowerCase()
+// }
 
-console.log(normalizarTexto(textao))
+// console.log(normalizarTexto(textao))
+
+// const input = document.querySelector('input');
+
+// const total = localStorage.getItem('total');
+
+// if (input && total) {
+//     input.value = total
+//     calcularGanho(Number(input.value))
+// }
+
+
+// function calcularGanho(value: number) {
+//     const p = document.querySelector('p');
+
+//     if (p) p.innerText = `ganho total: ${value + 100 - (value) * 0.2}`;
+// }
+
+// function totalMudou() {
+//     if (input) {
+//         const value = input.value;
+//         localStorage.setItem('total', value);
+//         calcularGanho(Number(value))
+
+//     }
+// }
+
+// if (input) input.addEventListener('keyup', totalMudou);
+
+// let total: number | string = 200
+
+// console.log(total)
+
+// const button = document.querySelector('button')
+
+// button?.click()
+
+// const toNumber = (value : number | string) => {
+//     if(typeof value === "string"){
+//         return Number(value)
+//     } else if (typeof value === "number"){
+//         return value
+//     } else{
+//         throw "value deve ser um número ou uma string"
+//     }
+// }
