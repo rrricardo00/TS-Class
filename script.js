2a01ce1ad5daecd68373b7bfdbdba4f40bc4a506
rrricardo00 <rrricardo00@yahoo.com.br>
2023-01-04 02:46:00 -0300

    ts

--- script.js
+++ script.js
@@ -159,3 +159,28 @@
 //     </div>
 //   `;
 // }
+class Livro {
+    autor;
+    constructor(autor) {
+        this.autor = autor;
+    }
+}
+class Jogo {
+    jogadores;
+    constructor(jogadores) {
+        this.jogadores = jogadores;
+    }
+}
+function buscarProduto(busca) {
+    if (busca === "O Hobbit") {
+        return new Livro('J.R.R.Tolkien');
+    }
+    if (busca === "Dark Souls") {
+        return new Jogo(1);
+    }
+    return null;
+}
+const produto = buscarProduto("O Hobbit");
+if (produto instanceof Livro) {
+    console.log(produto?.autor);
+}
--- script.ts
+++ script.ts
@@ -204,3 +204,38 @@
 //   `;
 // }
 
+
+class Livro {
+    autor: string
+
+    constructor(autor: string) {
+        this.autor = autor
+    }
+}
+
+class Jogo {
+    jogadores: number
+
+    constructor(jogadores: number) {
+        this.jogadores = jogadores
+    }
+}
+
+
+function buscarProduto(busca: string){
+    if(busca === "O Hobbit"){
+        return new Livro('J.R.R.Tolkien')
+    }
+
+    if(busca === "Dark Souls"){
+        return new Jogo(1)
+    }
+
+    return null
+}
+
+const produto = buscarProduto("O Hobbit")
+
+if(produto instanceof Livro){
+   console.log(produto?.autor)
+}
